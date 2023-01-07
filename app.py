6d9448e1897ae4fa91bf88246d3eda5c673756cd
whatcoloris <brian.mcdorkle@gmail.com>
2023-01-07 09:41:50 -0500

    a different request method

--- app.py
+++ app.py
@@ -12,24 +12,25 @@ user_token = os.environ.get("USER_TOKEN")
 
 d = discogs_client.Client('ExampleApplication/0.1', user_token=user_token)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        catnum = request.form['catnum']
-        artist = request.form['artist']
-        title = request.form['title']
-        label = request.form['label']
-        media = request.form['format']
-        rating = request.form['rating']
-        release = request.form['release']
-        release_id = request.form['release_id']
-
-        # Make a request to the API using the user's input
-        results = d.search(title, release=release, artist=artist, label=label, format=media, catno=catnum, rating=rating)
-        #for release in results:
-        #    return release.title
-        return render_template('results.html', results=results)
     return render_template('index.html')
+
+@app.route("/results")
+def results():
+    catnum = request.args.get('catnum')
+    artist = request.args.get('artist')
+    title = request.args.get('title')
+    label = request.args.get('label')
+    media = request.args.get('format')
+    rating = request.args.get('rating')
+    release = request.args.get('release')
+    release_id = request.args.get('release_id')
+
+    # Make a request to the API using the user's input
+    results = d.search(title, release=release, artist=artist, label=label, format=media, catno=catnum, rating=rating)
+    
+    return render_template('results.html', results=results)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
