8eebb53ef9bbe90e5910f1764d764268c4ac5989
Lami <41260099+tolem@users.noreply.github.com>
2023-01-11 22:44:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 from flask import Flask
 
 import os
-
+import urllib3
 import discogs_client
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 user_token = os.environ.get("USER_TOKEN")
-print(user_token)
+http = urllib3.PoolManager()
+r = http.request('GET', 'https://musicbrainz.org/ws/2/') # grab music information from musicbrainz
+
 
 d = discogs_client.Client('ExampleApplication/0.1', user_token=user_token)
 
 @app.route("/")
 def find_result():
     results = d.search('SHAKER LOOPS', type='release')
+
     for release in results:
         return release.title
 
-<<<<<<< HEAD
 
 @app.route("/home")
 def index():
     pass
-
-=======
-@app.route("/home")
-def index():
-    pass
->>>>>>> 2050b1a95c573fd27d67660c638c2454210bd43e
