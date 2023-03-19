e29c604969907d17145a04923ee380339939dd4a
Lami <olamide2olowoniyi@gmail.com>
2023-03-19 15:35:56 -0400

    basic musicbrian query tool that returns name from musicbrian db

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask.views import MethodView
-
 import os
 import urllib3
 import discogs_client
+import musicbrainzngs as mz
+import logging
 
 
 app = Flask(__name__)
 user_token = os.environ.get("USER_TOKEN")
+user_email = os.environ.get("USER_EMAIL")
+print(os.environ.get("USER_PASSWORD"), user_email)
+# musivbrains config
+mz.auth(os.environ.get("USER_NAME"),  os.environ.get("USER_PASSWORD")) # musicbrain authentication
+mz.set_useragent(app="api-tester", version="0.0.1", contact=user_email)
+mz.set_format(fmt='json') # sets the returned format to json
+
 http = urllib3.PoolManager()
 r = http.request('GET', 'https://musicbrainz.org/ws/2/') # grab music information from musicbrainz
 
@@ -29,10 +37,11 @@ class MusicApiView(MethodView):
 
     def post(self):
         data = request.form.get("title")
-        music_type = ""
-        results = d.search(data, type='release')
-        print(type(results))
-        return render_template("index.html", content=results)
+        music_type = "" 
+        d_results = d.search(data, type='release') # discogs results
+        mzb_results = mz.search_artists(query=data, limit=5, offset=None, strict=False, sortname=True) # searching musicbrains database
+        print(type(d_results), mzb_results)
+        return render_template("index.html", content=mzb_results)
         # return redirect(url_for("/music/"))
 
 
